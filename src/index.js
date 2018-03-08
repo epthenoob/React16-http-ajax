1c2c98610e33fe5f170917f989c265cca5225e4e
Eko <eko.pratama142213@gmail.com>
2018-03-08 14:18:18 +0700

    Final Axios project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// To get request globally to all file in the project
+axios.interceptors.request.use(requestData => {
+    console.log(requestData);
+
+    // Edit request config
+    return requestData
+}, error => {
+    // Error wich show when there's no internet access
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseData => {
+    console.log(responseData);
+
+    // Edit request config
+    return responseData
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
